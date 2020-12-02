70e4b527d5c3295106891ef26a346e55dc83c67d
Danny Brandt <dvbrandt90@gmail.com>
2020-12-02 03:33:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,28 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
+  
+def won?(board, player)
+ 
+end
+
+def full?(board)
+  board.none?(" ")
+end
+
+def draw?(boad)
+  if full?(board) && !won?(board)
+    true
+  else false 
+  end
+end
